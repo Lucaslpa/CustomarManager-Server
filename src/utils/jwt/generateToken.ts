9ba9548ee36f4cc1aed9e3faf9480b0ef77d8c24
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-10-12 12:11:48 -0300

    add jwt class

--- src/utils/jwt/generateToken.ts
+++ src/utils/jwt/generateToken.ts
@@ -1,6 +1,21 @@
 import jwt from 'jsonwebtoken';
 
-export const JWTgenerateToken = (payload: object) => {
-  const token = jwt.sign(payload, `${process.env.SECRET_KEY}`);
-  return token;
-};
+export class JWT {
+  generateToken = (payload: object) => {
+    try {
+      const token = jwt.sign(payload, `${process.env.SECRET_KEY}`);
+      return token;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  verifyToken = (token: string) => {
+    try {
+      const tokenIsValid = jwt.verify(token, `${process.env.SECRET_KEY}`);
+      return tokenIsValid;
+    } catch (e) {
+      return false;
+    }
+  };
+}
--- src/utils/jwt/jwt.test.ts
+++ src/utils/jwt/jwt.test.ts
@@ -1,12 +1,23 @@
-import { JWTgenerateToken } from './generateToken';
+import { JWT } from './generateToken';
 
+const jwt = new JWT();
+const payload = {
+  id: 20,
+  username: 'sdsd',
+};
 describe('jwt', () => {
-  it('should generate a token', () => {
-    const payload = {
-      id: 20,
-      username: 50
-    };
-    const res = JWTgenerateToken(payload);
-    console.log(res);
+  it('should generate a valid token', () => {
+    const token = jwt.generateToken(payload);
+    if (typeof token === 'string') {
+      const res = jwt.verifyToken(token);
+      expect(res).toBeTruthy();
+    }
+  });
+  it('should cause error if token is invalid', () => {
+    const token = jwt.generateToken(payload);
+
+    const invalidToken = `${token}1`;
+    const res = jwt.verifyToken(invalidToken);
+    expect(res).toBeFalsy();
   });
 });
