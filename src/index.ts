1921c37d1868832cef21d53422affda98da4f52c
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-11-01 18:45:33 -0300

    add CORS ENV

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import router from './routes';
 require('dotenv').config();
 
 const App = express();
-App.use(cors({ origin: ['http://localhost:3000'] }));
+App.use(cors({ origin: [`${process.env.CLIENT_ALLOW_CORS}`] }));
 App.use(express.json());
 App.use(express.urlencoded({ extended: true }));
 App.use(router);
